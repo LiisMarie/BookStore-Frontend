bfb1f2d176a64c0dd42ea9a32b5b64e905231d49
Liis-Marie Kütt <likutt@ttu.ee>
2020-09-15 22:45:43 +0300

    #6 add pop-up modal for adding a book to the cart

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,15 @@
   <div id="app">
     <nav-bar :categories="categories" :purchase="amountOfItems"></nav-bar>
     <router-view :dataset="collections" :updateCart="addToCart" :cart="cart" :purchase="totalPrice" />
+
+    <add-to-cart-modal :title="productHeading" :cost="productCost" :picture="productPicture"/>
   </div>
 </template>
 
 <script>
   import Navbar from './components/common_components/Navbar'
   import Data from './assets/book_list.json'
+  import AddToCartModal from "./components/common_components/AddToCartModal";
 
   export default {
     name: 'App',
@@ -15,7 +18,10 @@
       return {
         'collections': Data.collection,
         'categories': [],
-        'cart': []
+        'cart': [],
+        'productHeading': "",
+        'productCost': 0,
+        'productPicture': ""
       }
     },
     computed: {
@@ -34,7 +40,8 @@
       }
     },
     components: {
-      'nav-bar': Navbar
+      'nav-bar': Navbar,
+      'add-to-cart-modal': AddToCartModal
     },
     created () {
       this.categories = this.getCategories()
@@ -50,7 +57,13 @@
         return listedCategories
       },
       addToCart: function (product) {
-          this.cart.push(product)
+          if (!this.cart.includes(product)) {
+              this.cart.push(product)
+          }
+          this.productHeading = product.heading
+          this.productCost = product.cost
+          this.productPicture = product.picture
+          this.$bvModal.show("addToCartModal")
       }
     }
   }
