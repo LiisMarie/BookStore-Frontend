323ffc5abef4bd8f2a106b66aed27c6d80869ec0
Liis-Marie Kütt <likutt@ttu.ee>
2020-09-16 13:17:58 +0300

    #14 make navbar sticky

--- src/components/common_components/Navbar.vue
+++ src/components/common_components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <b-navbar toggleable="md" type="dark" variant="info">
+    <b-navbar sticky toggleable="md" type="dark" variant="info">
         <div class="container">
             <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
             <b-navbar-brand href="#" @click.prevent="route_to('/')"><font-awesome-icon :icon="['fas', 'book']"/> BookStore</b-navbar-brand>
@@ -10,13 +10,14 @@
                         <b-dropdown-item href="#" v-for="category in categories" :key="category" @click.prevent="route_to('/categories/'+category)">{{category}}</b-dropdown-item>
                     </b-nav-item-dropdown>
                 </b-navbar-nav>
-                <!-- Right aligned nav items -->
-                <b-navbar-nav class="ml-auto">
-                    <b-button variant="primary" @click="checkout" right>
-                        <font-awesome-icon :icon="['fas', 'shopping-cart']"/> {{purchase}}
-                    </b-button>
-                </b-navbar-nav>
             </b-collapse>
+
+          <b-navbar-nav is-nav class="ml-auto mr-1 float-right">
+              <a class="nav-link ml-auto mr-1" @click.prevent="route_to('/checkout')">
+                <font-awesome-icon :icon="['fas', 'shopping-cart']"/> {{purchase}}
+              </a>
+          </b-navbar-nav>
+
         </div>
     </b-navbar>
 </template>
@@ -26,9 +27,6 @@
         name: 'Navbar',
         props: ['categories', 'purchase'],
         methods: {
-            checkout () {
-                this.$router.push('/checkout')
-            },
             route_to (path) {
                 this.$router.push(path)
             }
@@ -37,5 +35,7 @@
 </script>
 
 <style scoped>
-
+  .b-navbar-brand {
+    position: absolute;
+  }
 </style>
