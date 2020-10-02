d857393bb30af8fe1f786195dcefc4492c73ff34
Liis-Marie Kütt <likutt@ttu.ee>
2020-10-02 23:05:37 +0300

    #21 show pop-up modal when purchase was successful

---
+++ src/components/checkout_components/MakeOrderModal.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-modal id="makeOrderModal"
+           centered
+           title="Your order has been received - Thank You!"
+           ok-title="Awesome"
+           ok-only>
+
+    <b-row>
+      <b-container>
+        <p>Your order has been successfully processed.</p>
+        <p>An email will be sent to You shortly.</p>
+        <b-img style="max-height:300px" fluid center src="../../assets/orderConfirmationPhoto.jpg"></b-img>
+      </b-container>
+
+    </b-row>
+
+  </b-modal>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+
+</style>
